3944e569b67123a14cb805099404871678af887d
Tom McKeesick <tmck01@gmail.com>
2021-05-01 00:21:26 +1000

    Disable using max page size, getting duplicates

--- realestate_com_au/realestate_com_au.py
+++ realestate_com_au/realestate_com_au.py
@@ -74,11 +74,7 @@ class RealestateComAu(Fajita):
             query_variables = {
                 "channel": channel,
                 "page": page,
-                "pageSize": (
-                    min(limit, self._MAX_SEARCH_PAGE_SIZE)
-                    if limit
-                    else self._DEFAULT_SEARCH_PAGE_SIZE
-                ),
+                "pageSize": self._DEFAULT_SEARCH_PAGE_SIZE,
                 "localities": [{"searchLocation": location} for location in locations],
                 "filters": {
                     "surroundingSuburbs": surrounding_suburbs,
